f740199d67e09b5fcb797815f7989f02f07ffe62
Iwan Aucamp <aucampia@gmail.com>
2025-02-07 14:48:20 +0100

    main: checkpoint 20250207T144820

---
+++ 20250207-ruff_import_format/example/local.py
@@ -0,0 +1,14 @@
+import json
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Any
+
+import pytest
+from attr import dataclass
+
+from nomad_event_ingestor import EventConverter
+from nomad_event_ingestor.util import (
+    _guess_env,
+    camel_to_snake_case,
+    job_id_tags,
+)
